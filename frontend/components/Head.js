a015f119f123724d9d451c3390121e24e8717c67
Paul Vidal <paul.vidal@yahoo.fr>
2020-10-27 16:53:20 +0100

    add sharing image

--- frontend/components/Head.js
+++ frontend/components/Head.js
@@ -7,7 +7,7 @@ export default function CustomHead() {
       <link rel="icon" href="/logo.svg" />
       <meta property="og:title" content="Shared spotify" />
       <meta property="og:description" content="The best way to create playlist with songs multiple like!" />
-      <meta property="og:image" content={process.env.NEXT_PUBLIC_URL + "/logo.png"} />
+      <meta property="og:image" content={process.env.NEXT_PUBLIC_URL + "/share.png"} />
 
       {/* always allow referrer to exist */}
       <meta name="referrer" content="always" />
