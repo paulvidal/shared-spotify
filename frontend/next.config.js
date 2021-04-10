f146e0798831aa76c72e5b4c9743e768c62c1e43
Paul Vidal <paul.vidal@yahoo.fr>
2021-04-10 18:20:31 +0200

    do not use pwa in dev mode

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    disable: false,
+    disable: process.env.NODE_ENV === 'development',
     register: true
   }
 })
